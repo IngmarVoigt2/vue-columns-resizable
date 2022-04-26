5d6a7607475ba87a79d4c1e7318606114a28d895
Ingmar Voigt <ingmar.voigt@siemens-healthineers.com>
2022-04-26 16:15:53 +0200

    vuetify testing

--- src/App.vue
+++ src/App.vue
@@ -58,18 +58,77 @@
         </tbody>
       </table>
     </div>
+
+    <div class="container">
+      <div>Resize on Vuetify v-data-table</div>
+        <v-data-table :headers="headers" :items="desserts" v-columns-resizable />      
+    </div>
   </div>
+
+
 </template>
 
 <script>
 export default {
   name: 'app',
+  // from  https://vuetifyjs.com/en/components/data-tables/#usage
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        }
+      ]
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-  width: 400px;
+  width: 800px;
   margin-bottom: 20px;
 }
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueColumnsResizable from './plugins/vue-columns-resizable';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css';
 
+Vue.use(Vuetify);
 Vue.use(VueColumnsResizable);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify: new Vuetify(),  
   render: h => h(App),
 }).$mount('#app');
